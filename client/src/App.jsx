a5eb92e5522c26505a472f08705e96ec075639e8
jadethegemstone <143663485+jadethegemstone@users.noreply.github.com>
2025-04-18 21:35:52 -0700

    Made CountrySelect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <Router basename='/'>
       <Suspense fallback={<div>Loading...</div>}>
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '90vh'}}>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
         <div style={{flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
           <Routes>
             <Route path="/" element={
