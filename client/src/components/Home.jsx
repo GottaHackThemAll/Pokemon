3dcde97b82b7fbffadc56c26ded530c892e15ce1
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-04-18 20:53:49 -0700

    home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Navigate, useNavigate } from 'react-router-dom';
 const Home = () => {
     const navigate = useNavigate(); 
@@ -7,6 +8,21 @@ const Home = () => {
             <button onClick={() => navigate('/CountrySelect')}>
                 Go to country select
             </button>
+=======
+import { useNavigate } from 'react-router-dom';
+import duocook from '../assets/duocook.png';
+const Home = () => {
+    const navigate = useNavigate(); 
+    return (
+        <div className='d-flex'>
+            <div className='d-flex flex-column justify-content-center w-50 text-start align-items-center ml-3'>
+                <div className='home home-header pl-2'>Welcome to CookingLingo!</div>
+                <button className='home green-btn home-header-btn px-2 align-self-start' onClick={() => navigate('/CountrySelect')}>
+                    Start your cooking Journey
+                </button>
+            </div>
+            <img src={duocook}/>  
+>>>>>>> Stashed changes
         </div>
         
     );
